3ac0c8c1198c27dae2ac433a3510fc3e6ba7dc5a
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-08-25 12:21:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-	return EXIT_SUCCESS;
-}
+    int cases=0;
+    int jumps=0;
+    cin>>cases;
+    for(int i=0; i < cases; i++){
+        int high = 0;
+        int low = 0;
 
+        cin>>jumps;
+        int heights[jumps];
+
+        for(int j=0; j<jumps; j++){
+            cin>>heights[j];
+        }
+
+        for(int j=0; j<jumps; j++){
+            if(j!=jumps-1){
+                if(heights[j]<heights[j+1]){
+                    high+=1;
+                }
+                else if(heights[j]>heights[j+1]){
+                    low+=1;
+                }
+            }
+        }
+
+        cout<<"Case "<< i+1<< ": "<<high<<" "<<low<<'\n';
+    }
+
+    return 0;
+}
